6c0bc383b4a81758deb7d7d3cd6c0b1342b57632
Patrick Cloke <clokep@gmail.com>
2015-05-11 09:10:34 -0400

    Clean-up the fabfile.

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import *
+from fabric.api import env, local, task
 import os
 
 # Local path configuration (can be absolute or relative to fabfile)
@@ -6,32 +6,56 @@ env.deploy_path = 'output'
 env.listen_port = 8000
 DEPLOY_PATH = env.deploy_path
 
+
+@task
 def clean():
+    """Delete any built output."""
     if os.path.isdir(DEPLOY_PATH):
         local('rm -rf {deploy_path}'.format(**env))
         local('mkdir {deploy_path}'.format(**env))
 
+
+@task
 def build():
+    """Build the blog."""
     local('pelican -s pelicanconf.py content')
 
+
+@task
 def rebuild():
+    """Clean, then build the blog."""
     clean()
     build()
 
+
+@task
 def regenerate():
+    """Watch files and continually build the blog as changes occur."""
     local('pelican -r -s pelicanconf.py content')
 
+
+@task
 def serve():
+    """Locally serve the blog."""
     local('cd {deploy_path} && python ../fake_server.py {listen_port}'.format(**env))
 
+
+@task
 def reserve():
+    """First build the blog, then serve it."""
     build()
     serve()
 
+
+@task
 def preview():
+    """Clean then build with the publish config."""
     clean()
     local('pelican -s publishconf.py content')
 
+
+@task
 def publish():
+    """Build with the publish config and push to the remote server."""
     preview()
     local('ghp-import -p -b master output')
