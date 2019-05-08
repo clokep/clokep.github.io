ff50f4bafd4f9b1affb5a44639f658136eae729f
Patrick Cloke <clokep@patrick.cloke.us>
2019-05-08 12:52:23 -0400

    Python 3 compatibility.

--- filters.py
+++ filters.py
@@ -5,7 +5,7 @@ def chunk(sequence, num_chunks):
     # From https://stackoverflow.com/questions/2130016/splitting-a-list-into-n-parts-of-approximately-equal-length
     """
     k, m = divmod(len(sequence), num_chunks)
-    return (sequence[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in xrange(num_chunks))
+    return (sequence[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(num_chunks))
 
 
 def paginate(page):
